f53fabf8b5f2ca272dcd65394a361c9ab859096d
Sahil Dua <sahildua2305@gmail.com>
2016-06-21 20:51:39 +0530

    Added support for windows

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: sahildua2305
 * @Date:   2016-06-21 19:14:10
 * @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-06-21 20:45:29
+* @Last Modified time: 2016-06-21 20:50:53
 */
 
 'use strict';
@@ -24,16 +24,22 @@ module.exports = mem(() => {
   const envVar = getEnvVar();
 
   if (envVar) {
-    // console.log(envVar);
-    // console.log(envVar === 'root');
-    return envVar === 'root';
+    return (envVar === 'root');
   }
+
+  var username;
 
   try {
     if (process.platform === 'darwin' || process.platform === 'linux') {
-      var username = cleanOutput(exec('id', ['-un'], {encoding: 'utf8'}));
-      console.log(username);
-      return username;
+      username = cleanOutput(exec('id', ['-un'], {
+        encoding: 'utf8'
+      }));
+      return (username === 'root');
+    } else if (process.platform === 'win32') {
+      username = cleanOutput(exec('whoami', {
+        encoding: 'utf8'
+      }));
+      return (username === 'root');
     }
   } catch (err) {}
 });
--- test/test-fallback.js
+++ test/test-fallback.js
@@ -2,7 +2,7 @@
 * @Author: sahildua2305
 * @Date:   2016-06-21 19:23:11
 * @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-06-21 20:41:30
+* @Last Modified time: 2016-06-21 20:51:02
 */
 
 'use strict';
@@ -12,5 +12,5 @@ import test from 'ava';
 test.serial('checkroot() when environment variables are not set', async t => {
   process.env.USER = '';
   process.env.LOGNAME = '';
-  t.is(require('../')(), 'sahildua2305');
+  t.is(require('../')(), false);
 });
